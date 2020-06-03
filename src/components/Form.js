ca6a5653e3aa8d448dbac9ab8f8ae9b99a6fbaac
AlanAlv <alan.alv96@gmail.com>
2020-06-03 03:37:04 -0500

    useCoin Custom Hook

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from '@emotion/styled';
 import useCoin from '../hooks/useCoin';
+import useCryptocoin from '../hooks/useCryptocoin';
+import axios from 'axios';
 
 const Button = styled.input`
     margin-top: 20px;
@@ -22,18 +24,35 @@ const Button = styled.input`
 
 const Form = () => {
 
+    // Cryptocoin state
+    const [ cryptoList, saveCryptocoins ] = useState([]);
+
     const COINS = [
         {code: 'USD', name: 'American Dollar'},
         {code: 'MXN', name: 'Mexican Peso'},
         {code: 'EUR', name: 'Euro'},
         {code: 'GBP', name: 'British Pound'}
     ]
+
     const [coin, SelectCoin] = useCoin('Choose your coin', '', COINS);
 
+    const [cryptocoin, SelectCryptocoin] = useCryptocoin('Choose your Cryptocoin', '', cryptoList);
+
+    // API call
+    useEffect(() => {
+        const callAPI = async () => {
+            const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD';
+            const result = await axios.get(url);
+
+            saveCryptocoins(result.data.Data);
+        }
+        callAPI()
+    }, []);
     return (  
         <form>
 
             <SelectCoin />
+            <SelectCryptocoin />
             <Button
                 type="submit"
                 value="Calculate"
---
+++ src/hooks/useCryptocoin.js
@@ -0,0 +1,54 @@
+import React, { Fragment, useState } from 'react';
+import styled from '@emotion/styled';
+
+const Label = styled.label`
+    font-family: 'Bebas Neue', cursive;
+    color: #FFF;
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 2.4rem;
+    margin-top: 2rem;
+    display: block;
+`;
+
+const Select = styled.select`
+    width: 100%;
+    display: block;
+    padding: 1rem;
+    -webkit-appearance: none;
+    border-radius: 10px;
+    border: none;
+    font-size: 1.2rem;
+`;
+
+const useCryptocoin = (label, initialState, options ) => {
+    //console.log(options);
+
+    // Custom hook state
+    const [ state, updateState ] = useState(initialState);
+
+    const SelectCryptocoin = () => (
+        <Fragment>
+            <Label>{label}</Label>
+            <Select
+                onChange={e => updateState(e.target.value)}
+                value={state}
+            >
+                <option value="">-- Select --</option>
+                {options.map(option => (
+                    <option 
+                        key={option.CoinInfo.Id} 
+                        value={option.CoinInfo.Name}
+                    >
+                        {option.CoinInfo.FullName}
+                    </option>
+                ))}
+            </Select>
+        </Fragment>
+    )
+
+    // Return state, interface and func to modify state
+    return [state, SelectCryptocoin, updateState];
+}
+
+export default useCryptocoin;
